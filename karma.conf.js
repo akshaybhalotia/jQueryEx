843bf988ee2f51705a2caa7c45311bb5d8057ede
Your Name <you@example.com>
2015-12-21 20:30:52 +0530

    Put the file structure and get jQuery working with karma

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,14 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine-jquery', 'jasmine'],
+    frameworks: ['jquery-2.1.3', 'jasmine-jquery', 'jasmine'],
+    
+
+    // plugins: [
+    //     require('karma-jquery'),
+    //     require('karma-jasmine-jquery'),
+    //     require('karma-jasmine')
+    //     ],
 
 
     // list of files / patterns to load in the browser
